b0907d41f535c4d25f6a6e9d4797e189afafd424
Matthew Hogan <mhogan@prndl.us>
2014-02-22 22:49:09 -0500

    Returns success

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 function submitLogin() {
 	console.log("hello")
-	$.ajax({
-		type: "POST",
-		url: "https://securis-debug.herokuapp.com/api/sign_in",
-		data: {'user_login': $("#my_form").serialize()}
+	
+	// Need to be cleaned before POSTing
+	params = {
+		"email" : $("#my_form input[name=email]").val(),
+		"password" : $("#my_form input[name=password]").val()
+	}
+	console.log(params);
+	$.post("https://securis-debug.herokuapp.com/api/sign_in", {
+		'user_login': params
 	}, function(data) {
 		console.log(data)
 	});
